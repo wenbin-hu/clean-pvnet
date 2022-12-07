d1c70187881df0e7d05a2a1629524769e8b41483
wenbin-hu <wenbin.hu@ed.ac.uk>
2022-12-07 17:30:08 +0800

    cv2 uses BGR while matplotlib uses RGB

--- tools/plot.py
+++ tools/plot.py
@@ -23,22 +23,22 @@ if __name__ == '__main__':
     corner_3d = get_model_corners(model)
 
     # load the data
-    i = 888
-    rgb = cv2.imread(rgb_dir + '/%d.jpg' % i)
+    i = 240
+    bgr = cv2.imread(rgb_dir + '/%d.jpg' % i)
     mask = cv2.imread(mask_dir + '/%d.png' % i)
     pose = np.load(pose_dir + '/pose%d.npy' % i, allow_pickle=True)
     corner_2d = pvnet_pose_utils.project(corner_3d, K, pose)
     fps_2d = pvnet_pose_utils.project(fps_3d, K, pose)
     
     # if the object is out of view and truncated, discard and redo the process
-    if max(fps_2d[:, 0]) >= rgb.shape[1] or max(fps_2d[:, 1]) >= rgb.shape[0] or \
+    if max(fps_2d[:, 0]) >= bgr.shape[1] or max(fps_2d[:, 1]) >= bgr.shape[0] or \
             min(fps_2d[:, 0]) < 1 or min(fps_2d[:, 1]) < 1:
         print("Object out of view")
     
     # plot
     _, axs = plt.subplots(1, 2)
     axs[0].imshow(mask)
-    axs[1].imshow(rgb)
+    axs[1].imshow(cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB))
     axs[1].plot(fps_2d[:, 0], fps_2d[:, 1], 'go', linewidth=2, markersize=3)
     axs[1].add_patch(patches.Polygon(xy=corner_2d[[0, 1, 3, 2, 0, 4, 6, 2]], fill=False, linewidth=1, edgecolor='b'))
     axs[1].add_patch(patches.Polygon(xy=corner_2d[[5, 4, 6, 7, 5, 1, 3, 7]], fill=False, linewidth=1, edgecolor='b'))
